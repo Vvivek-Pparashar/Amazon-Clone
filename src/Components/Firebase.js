b0b597f6c5ce31f75a6c58c298a2e319e05646dc
Vivek Parashar <96679102+Vvivek-Pparashar@users.noreply.github.com>
2022-10-11 00:26:54 +0530

    updated

---
+++ src/Components/Firebase.js
@@ -0,0 +1,25 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyDr2q4iWqTBr3zyvDqA4bVyotapc_QCei8",
+  authDomain: "clone-f3f23.firebaseapp.com",
+  projectId: "clone-f3f23",
+  storageBucket: "clone-f3f23.appspot.com",
+  messagingSenderId: "121553548711",
+  appId: "1:121553548711:web:56120c8b694016571e3282",
+  measurementId: "G-W3FY2Z2YPY",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
+
+// const auth = firebase.auth();
+
+// export {auth};
--- src/Components/Login.js
+++ src/Components/Login.js
@@ -1,9 +1,50 @@
-import React from 'react'
+import React from "react";
+import { Link } from "react-router-dom";
+import "./Login.css";
 
 const Login = () => {
-  return (
-    <div>Login</div>
-  )
-}
 
-export default Login
+  const login = (event)=>{
+    event.preventDefault();
+    console.log('first')
+  }
+
+  const register = (event)=>{
+    event.preventDefault();
+    console.log('register')
+  }
+  return (
+    <div className="login">
+      <Link to="/">
+        <img
+          src="https://upload.wikimedia.org/wikipedia/commons/thumb/a/a9/Amazon_logo.svg/1024px-Amazon_logo.svg.png"
+          className="login__logo"
+          alt="Logo"
+        />
+      </Link>
+      <div className="login__container">
+        <h1>Sign In</h1>
+        <form>
+          <h5>Email</h5>
+          <input type="email" />
+          <h5>Password</h5>
+          <input type="password" />
+          <Link to="/">
+            <button className="login__signInButton" type="submit" onClick={login}>
+              Sign in
+            </button>
+          </Link>
+        </form>
+        <p>
+          By continuing, you agree to Amazon's Conditions of Use and Privacy
+          Notice.
+        </p>
+        <button className="login__registerButton" onClick={register}>
+          Create your Amazon account
+        </button>
+      </div>
+    </div>
+  );
+};
+
+export default Login;
